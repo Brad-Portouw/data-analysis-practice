a4fb7cca3a2aa93f34b0ea2c951f1f3b17bdc9b8
Brad-Portouw <bportouw337@gmail.com>
2025-01-25 19:51:29 -0700

    Made a directory for cheat sheets for python data analysis. Also made a regex syntax to parse hashtags from colorado senators tweets.

--- hw/ch_6/ch_6_StringsDatesTidying.py
+++ hw/ch_6/ch_6_StringsDatesTidying.py
@@ -25,11 +25,22 @@ print(sen_tweets.info())
 # After importing the csv of senator tweets from 2011 to 2017, the intention is to analyze tweets made by colorado senators.
 # luckily there is a column for the state each senator is from.
 
-sen_tweets_CO = sen_tweets.query('state == "CO"')
+# Boolean expression involving the columns can sort out the colorado senators:
 
-print(sen_tweets_CO.info())
+CO_tweets = sen_tweets[sen_tweets.state == "CO"]
+
+# .query method also works, using a boolean as a string.
+
+CO_tweets = sen_tweets.query('state == "CO"')
+
+
+print(CO_tweets.info())
 
 # It appears that there are a total of around 5436 tweets from Colorado senators in the csv
 
 # finding all the hashtags used by Colorado senators using a pattern.
 
+tag = 'i feel so #b1essed today'
+
+CO_hashtag = re.findall(r"#(\d|\w+)", tag)
+print(CO_hashtag)
