affb20b763a1bd42ff36a7650cac14b83ad358d6
Brad-Portouw <bportouw337@gmail.com>
2025-01-25 17:46:20 -0700

    Started analysis of a csv file containing tweet data from Colorado Senators from between 2011 and 2017. Python file and csv

---
+++ hw/ch_6/ch_6_StringsDatesTidying.py
@@ -0,0 +1,35 @@
+"""
+IN this exercise there is a focus on parsing strings for certain phrases and data. For this a CSV containing tweets from
+Colorado senators is used to descern information
+"""
+
+# Importing packages
+
+import math
+import collections
+import numpy as np
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+import datetime
+from matplotlib import style
+# importing regex module to parse strings
+import re
+
+# Importing senators.csv from the data directory.
+
+#
+sen_tweets = pd.read_csv('../../data/senators.csv', encoding='latin-1')
+print(sen_tweets)
+print(sen_tweets.info())
+# After importing the csv of senator tweets from 2011 to 2017, the intention is to analyze tweets made by colorado senators.
+# luckily there is a column for the state each senator is from.
+
+sen_tweets_CO = sen_tweets.query('state == "CO"')
+
+print(sen_tweets_CO.info())
+
+# It appears that there are a total of around 5436 tweets from Colorado senators in the csv
+
+# finding all the hashtags used by Colorado senators using a pattern.
+
