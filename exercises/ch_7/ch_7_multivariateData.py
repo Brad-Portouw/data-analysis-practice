2ef890fe390b4163f3b325766d557816437f3ad3
Brad-Portouw <bportouw337@gmail.com>
2025-01-29 21:44:36 -0700

    More work on ch7. WIP

--- exercises/ch_7/ch_7_multivariateData.py
+++ exercises/ch_7/ch_7_multivariateData.py
@@ -14,5 +14,37 @@ from matplotlib import style
 
 # Import the 'ME_salaries.csv' file from the data directory
 
-ME_sal = pd.read_csv('../../data/ME_salaries.csv', names=['Salary', 'Age', 'Gender'], skiprows=1)
-print(ME_sal)
+me_sal = pd.read_csv('../../data/ME_salaries.csv', names=['Salary', 'Age', 'Gender'], skiprows=1)
+print(me_sal.info())
+
+# Looking at respondents who didn't list their salary, pd.isnull isn't working to find empty arrays of "" recorded for salary.
+# However there are 15 respondents who answered having 0 for their salary:
+me_no_sal = me_sal.loc[me_sal['Salary'] == 0]
+
+# Therefore it would make sense to remove these responses at outliers, there could be many reasons why someone would list their salary at 0.
+me_sal = me_sal.loc[me_sal['Salary'] != 0]
+# Need to reset the index after removing some rows:
+me_sal.reset_index(drop=True, inplace=True)
+
+# Taking a look at the genders shown in the dataframe:
+print(me_sal.Gender.value_counts())
+
+# It appears that 3228 men and 373 women responded with no other possible gender identities being listed.
+
+# With the data imported, plots could provide more insight into each category.
+# print(me_sal.Age.value_counts())
+
+# ages = me_sal.sort_values('Age')
+age_count = me_sal['Age']
+age_count = age_count.sort_values(ascending=True)
+print(age_count)
+print(age_count.value_counts().sort_values(ascending=True))
+
+
+
+# plt.style.use('ggplot')
+# plt.bar(me_sal, color='b')
+# plt.title('Age of Mechanical Engineers National Science Foundation 2017')
+# plt.xlabel('Age')
+# plt.ylabel('Count')
+# plt.show()
