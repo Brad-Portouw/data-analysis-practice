f623df7248aa1a85ff71c5fa21d52afd1821cf05
Brad-Portouw <bportouw337@gmail.com>
2025-02-12 21:37:36 -0700

    A bit more analysis of the engineering salary data with a jupyter notebook copy.

--- exercises/ch_7/ch_7_multivariateData.py
+++ exercises/ch_7/ch_7_multivariateData.py
@@ -155,16 +155,98 @@ plt.show()
 # looking at both boxplots, they would imply that female engineers as a median have slighly lower salaries than their male counterparts.
 # It also appears that a larger porportion of women engineers are younger.
 
-# a scatterplot showing the age(x-axis) vs Salary may show trends between the two, where we would expect older engineers to be paid more.
+#a scatterplot showing the age(x-axis) vs Salary may show trends between the two, where we would expect older engineers to be paid more.
 # Having 1 plot containing both the data for men and women superimposed on one another is difficult to read, so two subplots seems to be a better option.
 
 fig, ax = plt.subplots(1, 2, sharey=True)
 ax[0].scatter(me_men['Age'], me_men['Salary'],
             c='xkcd:grey blue',
-            alpha= 0.8)
+            s=5,
+            alpha= 1)
 ax[1].scatter(me_women['Age'], me_women['Salary'],
             c='xkcd:light orange',
-            alpha= 0.8)
-ax[0]
-fig.suptitle('Age versus Salary Mechanical Engineers', fontsize=25)
+            s=5,
+            alpha= 1)
+ax[0].set_title('Men', fontsize=12)
+ax[0].set_ylim(0,500000)
+ax[0].set_yticks(list(range(0,500000,100000)), [f"${x}k" for x in salary_ticks])
+ax[0].set_xlim(20,80)
+ax[1].set_xlim(20,80)
+ax[1].set_title('Women', fontsize=15)
+fig.suptitle('Age versus Salary Mechanical Engineers', fontsize=20)
 plt.show()
+
+#This plot will have different readability depening upon the size of the window it is opened in.
+# A large number of datapoints makes it difficult to read if the points are too large due to overlap.
+# In a small preview window, the marker points are small
+# However, in a large window the marker points may need to be larger (s goes up) as they don't scale with the window itself.
+
+# A pearson coefficent here could show a correlation between age and salary.
+cor_eng, p = pearsonr(me_sal['Age'], me_sal['Salary'])
+cor_eng
+print(f'The Pearsons correlation between age and salary for mechanical engineers is %.3f' %cor_eng)
+
+# Checking for each gender now:
+cor_men, p_men = pearsonr(me_men['Age'], me_men['Salary'])
+print(f'The Pearsons correlation for age vs salary for men is %.3f' %cor_men )
+cor_women, p_women = pearsonr(me_women['Age'], me_women['Salary'])
+print(f'Likewise the Pearsons correlation for women is %.3f' %cor_women)
+
+# It would appear that the pearson correlation between age and salary has a slight positive value, and doesn't have a signifigant difference between men and women.
+# Looking at the scatterplot above, it appears that young engineers have entry level salaries that are grouped together, but as time goes on the salaries gain variance.
+
+# a cumulative distribuiton comparing salary may discern what salary the majority of engineers should expect in their careers.
+x_men = np.sort(me_men['Salary'])
+x_fem = np.sort(me_women['Salary'])
+
+y_men = np.arange(len(me_men))/ float(len(me_men))
+y_fem = np.arange(len(me_women))/ float(len(me_women))
+
+plt.plot(x_men,y_men, c='xkcd:grey blue')
+plt.plot(x_fem,y_fem, c='xkcd:light orange')
+# After a preliminary look at  the graph, the plot is affected by a couple of outliers with a few people making around 1 million dollars. In this case they will be removed from this plot
+plt.title('Cumulative Distribution of Engineering Salaries by Gender' )
+plt.xlabel('Engineering Salary')
+plt.ylabel('Cumulative Fraction')
+plt.xlim(0,500000)
+plt.xticks(list(range(0,500000,100000)), [f"${x}k" for x in salary_ticks])
+plt.yticks(np.arange(0,1.1,0.1))
+plt.legend(['Men', 'Women'])
+plt.show()
+
+# Looking at this plot, it appears that about 99% of engineers make less than 250k, so it may be beneficial to zoom in to notice differences more easily.
+sal_ticks2 = list(range(0,350,50))
+plt.plot(x_men,y_men, c='xkcd:grey blue')
+plt.plot(x_fem,y_fem, c='xkcd:light orange')
+# After a preliminary look at  the graph, the plot is affected by a couple of outliers with a few people making around 1 million dollars. In this case they will be removed from this plot
+plt.title('Cumulative Distribution of Engineering Salaries by Gender' )
+plt.xlabel('Engineering Salary')
+plt.ylabel('Cumulative Fraction')
+plt.xlim(0,300000)
+plt.xticks(list(range(0,350000,50000)), [f"${x}k" for x in sal_ticks2])
+plt.yticks(np.arange(0,1.1,0.1))
+plt.legend(['Men', 'Women'])
+plt.show()
+
+# This plot is rather unfortunate, which suggests that women are statistically less likely to make the same wage as their male counterparts
+# The quantiles were shown in the boxplot earlier, but calculating them could be useful
+
+# me_women.Salary.describe()
+# me_men.Salary.describe()
+quantile_f = me_women.Salary.quantile([0, 0.25, 0.5, 0.75, 1.0])
+quantile_m = me_men.Salary.quantile([0, 0.25, 0.5, 0.75, 1.0])
+print(f'The median salary for women is $%.0f' %quantile_f.iloc[2])
+print(f'The median salary for men is $%.0f' %quantile_m.iloc[2])
+print(f'The difference in the medians is: $%.0f' %(quantile_m.iloc[2]-quantile_f.iloc[2]))
+print(f'Maximum salary for women is $%.0f' %quantile_f.iloc[4])
+print(f'Maximum salary for men is $%.0f' %quantile_m.iloc[4])
+print(f'The difference in the maximum is $%.0f' %(quantile_m.iloc[-1]-quantile_f.iloc[-1]))
+
+'''
+There are a large number of potential factors in play for overall pay of employees, but the largest one might be the 
+company and occupation of each engineer. If a company and type  of engineering could be found, it would be helpful. Also
+it seems that the pay for many engineers plateaus at around 100k. It could be helpful to know which engineers have 
+management positions. The school each engineer graduated at may play a small role in salary, as new hires out of 
+prestigous schools may be offered more to start. Also where the engineer lives would play a role in pay considering cost 
+of living, and potential benefits. 
+'''
