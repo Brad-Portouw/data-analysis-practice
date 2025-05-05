c05619f46e1b7a6361aa3fd13bdfa7bda4c971c2
Brad-Portouw <bportouw337@gmail.com>
2025-05-05 16:39:21 -0600

    Created a chapter 11 directory with corresponding .py file for the exercise

---
+++ exercises/ch_11/ch_11_modeling.py
@@ -0,0 +1,8 @@
+'''
+This exercise is derived from coursework made by Professor John Volkens at Colorado State University
+Title: ch_11_modeling
+Written by: Brad Portouw
+Created: 5/5/25
+output: .py file
+'''
+
